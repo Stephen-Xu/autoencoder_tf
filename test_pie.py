996da6f58c1e2b35364875450ebefe5057d63439
ceru23 <ceru@CUDone.docenti.di.unimi.it>
2015-12-18 16:39:22 +0100

    test pie con pre train

--- test_pie.py
+++ test_pie.py
@@ -7,29 +7,30 @@ data = np.loadtxt("../datasets/multipie_rand_sel_space.dat")
 
 data = data+abs(np.min(data))
 data = data/np.max(data)
-
+data = data.astype("float32")
 int_dim = 100
 
-bat = batch.seq_batch(data,200)
+bat = batch.seq_batch(data,1000)
 
 #units = [data.shape[1],int(math.ceil(data.shape[1]*1.2))+5,int(max(math.ceil(data.shape[1]/4),int_dim+2)+3),
 #         int(max(math.ceil(data.shape[1]/10),int_dim+1)),int_dim]
 
-units = [5600,2000,200,100]
+units = [5600,2300,1100,600,200,100]
 
-act = ['sigmoid','sigmoid','sigmoid']
+act = ['sigmoid','sigmoid','sigmoid','sigmoid','sigmoid']
 #act = ['relu','relu','relu','relu']
 auto = autoencoder.autoencoder(units,act)
 
 auto.generate_encoder()
 auto.generate_decoder()
 
-auto.pre_train(data,n_iters=2000)
+auto.pre_train(data,n_iters=5000)
 
 session = auto.init_network()
 
-auto.train(data,batch=bat,le=False,tau=1.0,session=session,n_iters=2000,display=False,noise=False,noise_level=0.015)
+ic,bc = auto.train(data,batch=bat,le=False,tau=1.0,session=session,n_iters=2000,display=False,noise=False,noise_level=0.015)
 
+print 'Init: ',ic,' Best:',bc
 
 mid = auto.get_hidden(data,session=session)
 
