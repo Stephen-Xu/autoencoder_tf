0b21ed9f29de5ecb04afc4eb57db21787094fa55
ceru23 <ceru@CUDone.docenti.di.unimi.it>
2016-01-01 17:42:40 +0100

    test su mnist

--- test_minst.py
+++ test_minst.py
@@ -4,7 +4,7 @@ from autoencoder import autoencoder
 
 
 units = [784,10]
-action = ['softplus']
+action = ['tanh']
 
 l_rate =  0.01
 
@@ -16,8 +16,8 @@ arr = np.fromfile(f, '>u1', 60000 * 28 * 28).reshape((60000, 784))
 max_value = 0xFF
 
 arr = arr.astype(float)
-arr -= max_value / 2.0
-arr /= max_value
+#arr -= max_value / 2.0
+#arr /= max_value
 
 data = arr
 
@@ -29,5 +29,19 @@ auto.generate_encoder()
 auto.generate_decoder()
 
 
-auto.train(data,n_iters=1000,batch=None,display=False,noise=False,gradient=grad,learning_rate=l_rate)
+auto.train(data,n_iters=2000,model_name='model_tahnh.ckpt',batch=None,display=False,noise=False,gradient=grad,learning_rate=l_rate)
 
+auto2 = autoencoder(units,['sofftplus'])
+
+auto2.generate_encoder()
+auto2.generate_decoder()
+
+auto2.train(data,n_iters=2000,model_name='model_splus.ckpt',batch=None,display=False,noise=False,gradient=grad,learning_rate=l_rate)
+
+
+auto3 = autoencoder(units,['sigmoid'])
+
+auto3.generate_encoder()
+auto3.generate_decoder()
+
+auto3.train(data,n_iters=2000,model_name='model_sigmo.ckpt',batch=None,display=False,noise=False,gradient=grad,learning_rate=l_rate)
