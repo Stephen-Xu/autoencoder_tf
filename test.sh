7346426cae7bc369beadc083b34e3a585934de69
ceru23 <ceru@CUDone.docenti.di.unimi.it>
2016-01-14 17:06:10 +0100

    pre training rbm for mnist ok

--- test.sh
+++ test.sh
@@ -1,10 +1,10 @@
-python test_minst.py -a 'softplus' -u 10 -b 1 -g adam -l 0.000125 -p 0.000000125 -n 1000  -i 5000 -m ./model_softplus_10_batch1000_class0.ckpt
+python test_minst.py -a 'softplus' -u 10 -b 1 -g adam -l 0.000125 -p 0.00001 -n 1000  -i 5000 -m ./model_softplus_10_batch1000_class0.ckpt
 
-python test_minst.py -a 'sigmoid' -u 10 -b 1 -g adam -l 0.000125 -p 0.000125 -n 1000  -i 5000 -m ./model_sigmoid_10_batch1000_class0.ckpt
+python test_minst.py -a 'sigmoid' -u 10 -b 1 -g adam -l 0.000125 -p 0.001 -n 1000  -i 5000 -m ./model_sigmoid_10_batch1000_class0.ckpt
 
-python test_minst.py -a 'softplus' -u 80 -b 1 -g adam -l 0.000125 -p 0.000000125 -n 1000  -i 5000 -m ./model_softplus_80_batch1000_class0.ckpt
+python test_minst.py -a 'softplus' -u 80 -b 1 -g adam -l 0.000125 -p 0.00001 -n 1000  -i 5000 -m ./model_softplus_80_batch1000_class0.ckpt
 
-python test_minst.py -a 'sigmoid' -u 80 -b 1 -g adam -l 0.000125 -p 0.000125-n 1000 -i 5000 -m ./model_sigmoid_80_batch1000_class0.ckpt
+python test_minst.py -a 'sigmoid' -u 80 -b 1 -g adam -l 0.000125 -p 0.001 -n 1000 -i 5000 -m ./model_sigmoid_80_batch1000_class0.ckpt
 
 
 
