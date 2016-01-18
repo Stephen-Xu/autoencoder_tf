fd7fac00b4388c56e084246eeab86a269ddfe92c
ceru <ceru23@gmail.com>
2016-01-18 17:23:11 +0100

    plot weights with parsers

--- plot_weights.py
+++ plot_weights.py
@@ -1,12 +1,22 @@
 import numpy as np
 import sys
 import matplotlib.pyplot as plt
-        
+from optparse import OptionParser
+
+parser = OptionParser()
+parser.add_option("-f", "--file", dest="file",
+                  help="File where the weights are stored",default="./weights.txt")
+parser.add_option("-l", "--layer",
+                  dest="layer", default=0,
+                  help="Selected layer")
 
 
-r_w = np.loadtxt(sys.argv[1])
+(options, args) = parser.parse_args()
 
-l = int(sys.argv[2])
+
+r_w = np.loadtxt(options.file)
+
+l = int(options.layer)
 
 plt.plot([w[l] for w in r_w])
 
