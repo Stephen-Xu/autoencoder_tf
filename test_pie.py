652b5d0475d6575577eea153f5aec68b290b4a91
ceru23 <ceru@CUDone.docenti.di.unimi.it>
2016-02-08 10:31:25 +0100

    mod test pie

--- test_pie.py
+++ test_pie.py
@@ -1,21 +1,22 @@
 import autoencoder
 import math
 import numpy as np
+import sys
 from tools.data_manipulation import batch
 
-data = np.loadtxt("../datasets/multipie_rand_sel_space.dat")
+data = np.loadtxt("../datasets/multi_pie.dat")
 
 data = data+abs(np.min(data))
 data = data/np.max(data)
 data = data.astype("float32")
-int_dim = 100
+
 
 bat = batch.rand_batch(data,1000)
 
 #units = [data.shape[1],int(math.ceil(data.shape[1]*1.2))+5,int(max(math.ceil(data.shape[1]/4),int_dim+2)+3),
 #         int(max(math.ceil(data.shape[1]/10),int_dim+1)),int_dim]
 
-units = [5600,1100,200,100]
+units = [5600,1100,200,int(sys.argv[1])]
 
 act = ['sigmoid','sigmoid','sigmoid']
 #act = ['relu','relu','relu','relu']
@@ -27,15 +28,15 @@ auto.generate_decoder(symmetric=True)
 
 session = auto.init_network()
 
-ic,bc = auto.train(data,n_iters=5000,record_weight=True,w_file='./pie_weights',use_dropout=True,keep_prob=0.5,reg_weight=False,reg_lambda=0.0,model_name='./pie',batch=bat,display=False,noise=False,gradient='adam',learning_rate=0.0000125)
+ic,bc = auto.train(data,n_iters=5000,record_weight=False,w_file='./pie_weights_20',use_dropout=True,keep_prob=0.5,reg_weight=False,reg_lambda=0.0,model_name=sys.argv[2],batch=bat,display=False,noise=False,gradient='adam',learning_rate=0.0000125)
 
 
 print 'Init: ',ic,' Best:',bc
 
-mid = auto.get_hidden(data,session=session)
+#mid = auto.get_hidden(data,session=session)
 
-out = auto.get_output(data,session=session)
+#out = auto.get_output(data,session=session)
 
-np.save('middle.dat',mid)
-np.save('output.dat',out)
+#np.save('middle.dat',mid)
+#np.save('output.dat',out)
 
