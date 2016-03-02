b01f768c1360db9110ac254e24e366b2a9d4178b
ceru <ceru23@gmail.com>
2016-03-02 16:34:25 +0100

    class ffjei

--- test_class.py
+++ test_class.py
@@ -1,5 +1,6 @@
 from classifier import classifier
 import numpy as np
+from scipy import misc
 
 units = [24,48,96]   #################
 act = ['tanh','tanh']
@@ -11,5 +12,10 @@ cl.generate_classifier()
 
 session = cl.init_network()
 
-cl.train()
+#cl.train()
 
+image = misc.imread('./cat.jpg').astype("float32")
+
+r,o  = cl.test_model(image,session=session,model='./converted.mdl')
+
+print zip(r,o)
