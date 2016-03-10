04c56ffc525c52c049e2c6c554388f94e7479e5c
ceru <ceru23@gmail.com>
2016-03-10 13:06:09 +0100

    class 38

--- test_class.py
+++ test_class.py
@@ -8,45 +8,41 @@ from tools.image import display
 units = [24,72,120,120,96]   #################
 act = ['tanh','tanh','linear','linear']
 
+cl = classifier(units,act)
 
-with tf.Graph().as_default():
+cl.generate_classifier(std_w=3.0,euris=True,dropout=True,keep_prob_dropout=[1.0,0.5,0.5,1.0,1.0])
+session = cl.init_network()
 
-    cl = classifier(units,act)
-
-    cl.generate_classifier(std_w=3.0,euris=True,dropout=True,keep_prob_dropout=[1.0,0.5,0.5,1.0,1.0])
-
-    session = cl.init_network()
-
-    if(sys.argv[1]=='t'):
+if(sys.argv[1]=='t'):
 
     
-    	cl.train()
+  	cl.train()
 
-    else:
+else:
     
-        image = misc.imread('./cat.jpg').astype("float32")
+    image = misc.imread('./cat.jpg').astype("float32")
     
-        print np.mean(session.run(cl.layers[0].W))
+    print np.mean(session.run(cl.layers[0].W))
         
-        cl.stop_dropout()
+    cl.stop_dropout()
     
-        image = np.random.uniform(1,255,[7,7,3])
+    image = np.random.uniform(1,255,[7,7,3])
     
-        cl.load_model('./converted.mdl',session=session)
+    cl.load_model('./converted.mdl',session=session)
     
-        print np.mean(session.run(cl.layers[0].W))
+    print np.mean(session.run(cl.layers[0].W))
     
-        r,o,p  = cl.test_model(image,session=session)
+    r,o,p  = cl.test_model(image,session=session)
     
-        r = np.ndarray.flatten(r)
-        o = np.ndarray.flatten(o)
+    r = np.ndarray.flatten(r)
+    o = np.ndarray.flatten(o)
     #print zip(r,o)
-        print "dist:",0.5*np.sum((r-o)**2)
-        print "red norm:",np.linalg.norm(r)
-        print "ori norm:",np.linalg.norm(o)
+    print "dist:",0.5*np.sum((r-o)**2)
+    print "red norm:",np.linalg.norm(r)
+    print "ori norm:",np.linalg.norm(o)
     
-        for i in range(len(cl.layers)):
-            print "W",i+1,":",np.linalg.norm(session.run(cl.layers[i].W))
+    for i in range(len(cl.layers)):
+        print "W",i+1,":",np.linalg.norm(session.run(cl.layers[i].W))
 
    # p = np.squeeze(p)
     #display.display(p,p.shape[0],p.shape[1],c=3)
