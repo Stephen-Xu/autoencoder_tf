cccad2a972392bb392f0ad7792d73af46e480e83
ceru <ceru23@gmail.com>
2016-03-10 15:44:21 +0100

    graph 46

--- classifier.py
+++ classifier.py
@@ -9,9 +9,9 @@ import numpy as np
 
 FLAGS = tf.app.flags.FLAGS
 
-tf.app.flags.DEFINE_integer('iters',2,"""Number of iterations.""")
+tf.app.flags.DEFINE_integer('iters',50000,"""Number of iterations.""")
 tf.app.flags.DEFINE_string('model','./converted.mdl',"""File for saving model.""")
-tf.app.flags.DEFINE_integer('batch',10,"""Size of batches.""")
+tf.app.flags.DEFINE_integer('batch',500,"""Size of batches.""")
 tf.app.flags.DEFINE_integer('heigth',224,"""Height of images""")
 tf.app.flags.DEFINE_integer('width',224,"""Width of images""")
 tf.app.flags.DEFINE_string('path','/home/ceru/datasets/ILSVRC2012_VAL_SET/images/',"""Data folder""")
