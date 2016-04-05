c087b05ee121daad1ce5d2eb42079fa73d139b4a
ceru <ceru23@gmail.com>
2016-04-05 12:13:57 +0200

    test su vgg-16

--- test_conv.py
+++ test_conv.py
@@ -5,13 +5,13 @@ from tools.image import display
 import numpy as np
 import sys 
 
-data = np.loadtxt("conv")
+data = np.loadtxt("conv64")
 
-m = np.min(data)
+#m = np.min(data)
 
 #data = data+abs(m)
 
-units = [96,196,48,24]
+units = [64,128,48,24]
 act = ['tanh','tanh','tanh']
 act2 = ['linear','linear','linear']
 auto = autoencoder(units,act)
@@ -23,22 +23,22 @@ session = auto.init_network()
 
 bat = batch.rand_batch(data,70)
 
-ic,bc = auto.train(data,batch=bat,n_iters=5000,use_dropout=True,keep_prob=0.5,reg_weight=False,reg_lambda=0.0,model_name='conv_lin_24.mod',display=False,noise=False,gradient='adam',learning_rate=0.000125)
+ic,bc = auto.train(data,batch=bat,n_iters=5000,use_dropout=True,keep_prob=0.5,reg_weight=False,reg_lambda=0.0,model_name='conv_24.mod',display=False,noise=False,gradient='adam',learning_rate=0.000125)
 
 
 print "Finished. Initial cost: ",ic," Final cost: ",bc
 
-auto.load_model('conv_lin_24.mod',session=session)
+auto.load_model('conv_24.mod',session=session)
 #data = data+m
 auto.stop_dropout()
 red_feat = auto.get_hidden(data,session=session)
 out_feat = auto.get_output(data,session=session)
-np.savetxt("red_feat_lin_24",red_feat)
-np.savetxt("out_feat_lin_24",out_feat)
+np.savetxt("red_24",red_feat)
+np.savetxt("out_24",out_feat)
 
 nd = np.transpose(data)
 no = np.transpose(out_feat)
 if(sys.argv[1]=='s'):
     for i in range(96):
         #display.save(nd[i],7,7,3,folder='./feat',name='lin_in_24',index=i)
-        display.save(no[i],7,7,3,folder='./feat',name='lin_out_24',index=i)
+        display.save(no[i],7,7,3,folder='./feat',name='out_24',index=i)
