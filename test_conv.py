25d2a35bc225806a5a7714f92bc27de38aaaa819
ceru <ceru23@gmail.com>
2016-04-05 18:27:49 +0200

    test vgg16

--- test_conv.py
+++ test_conv.py
@@ -1,6 +1,6 @@
 import tensorflow as tf
 from autoencoder import autoencoder
-from tools.data_manipulation import batch
+#from tools.data_manipulation import batch
 from tools.image import display
 import numpy as np
 import sys 
@@ -11,7 +11,7 @@ data = np.loadtxt("conv64")
 
 #data = data+abs(m)
 
-units = [64,128,48,24]
+units = [64,128,42,7]
 act = ['tanh','tanh','tanh']
 act2 = ['linear','linear','linear']
 auto = autoencoder(units,act)
@@ -21,24 +21,24 @@ auto.generate_decoder(symmetric=False,act=act2)
 
 session = auto.init_network()
 
-bat = batch.rand_batch(data,70)
+#bat = batch.rand_batch(data,70)
 
-ic,bc = auto.train(data,batch=bat,n_iters=5000,use_dropout=True,keep_prob=0.5,reg_weight=False,reg_lambda=0.0,model_name='conv_24.mod',display=False,noise=False,gradient='adam',learning_rate=0.000125)
+ic,bc = auto.train(data,batch=None,n_iters=5000,use_dropout=False,keep_prob=0.5,reg_weight=False,reg_lambda=0.0,model_name='conv_7.mod',display=False,noise=False,gradient='adam',learning_rate=0.000125)
 
 
 print "Finished. Initial cost: ",ic," Final cost: ",bc
 
-auto.load_model('conv_24.mod',session=session)
+auto.load_model('conv_7.mod',session=session)
 #data = data+m
 auto.stop_dropout()
 red_feat = auto.get_hidden(data,session=session)
 out_feat = auto.get_output(data,session=session)
-np.savetxt("red_24",red_feat)
-np.savetxt("out_24",out_feat)
+np.savetxt("red_7",red_feat)
+np.savetxt("out_7",out_feat)
 
 nd = np.transpose(data)
 no = np.transpose(out_feat)
 if(sys.argv[1]=='s'):
-    for i in range(96):
-        #display.save(nd[i],7,7,3,folder='./feat',name='lin_in_24',index=i)
-        display.save(no[i],7,7,3,folder='./feat',name='out_24',index=i)
+    for i in range(64):
+        #display.save(nd[i],3,3,3,folder='./feat',name='in_12',index=i)
+        display.save(no[i],3,3,3,folder='./feat',name='out_7',index=i)
